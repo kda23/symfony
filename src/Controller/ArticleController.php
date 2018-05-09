82bc88e143501f3cf1dc035ea0759702f1f3bf1a
kda23 <kda.23@mail.com>
2018-05-10 01:35:54 +0300

    new controller test

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf('Future page to show the article: %s', $slug));
+    }
+}
