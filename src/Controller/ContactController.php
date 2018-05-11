8d48f921b9a9cd3be0ee78c6d69c9efa7b457894
kda23 <kda.23@mail.com>
2018-05-11 19:34:33 +0300

    Pages are working. Added controllers / pages .twig for view

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class ContactController extends Controller
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index()
+    {
+        return $this->render('main/contact.html.twig', [
+            'controller_name' => 'ContactController',
+            'test' => 'test',
+        ]);
+    }
+}
---
+++ src/Controller/NewsController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class NewsController extends Controller
+{
+    /**
+     * @Route("/news", name="news")
+     */
+    public function index()
+    {
+        return $this->render('main/news.html.twig', [
+            'controller_name' => 'NewsController',
+            'test' => 'test',
+        ]);
+    }
+}
